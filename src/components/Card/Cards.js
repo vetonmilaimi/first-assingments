62d2812dbee310dc61e71fe55706b3d8d666a61c
Veton Milaimi <vetonmila@gmail.com>
2022-01-28 00:11:12 +0100

    Add more cards

--- src/components/Card/Cards.js
+++ src/components/Card/Cards.js
@@ -24,6 +24,48 @@ const DUMMY_CARDS = [
         name: "Kevin Joe",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
     },
+        {
+        id: "4",
+        date: "3.3.0 (14/05/2018)",
+        what: "New",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    },
+    {
+        id: "5",
+        date: "3.1.0 (20/05/2015)",
+        what: "Fix",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    },
+    {
+        id: "6",
+        date: "3.1.0 (20/05/2015)",
+        what: "Improvement",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    },
+        {
+        id: "7",
+        date: "3.3.0 (14/05/2018)",
+        what: "New",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    },
+    {
+        id: "8",
+        date: "3.1.0 (20/05/2015)",
+        what: "Fix",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    },
+    {
+        id: "9",
+        date: "3.1.0 (20/05/2015)",
+        what: "Improvement",
+        name: "Kevin Joe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In consectetur adipiscing consectetur adipiscing elit. In varius faucibus massa, elit. In varius faucibus massa, varius faucibus massa, non commodo mauris luctus ut. consectetur adipiscing elit. In varius faucibus massa, Vestibulum quis mollis sem, vitae placerat nunc."
+    }
 ];
 
 const data = DUMMY_CARDS.map((card) => {
